cd9a24762a09c683469945d48f775f5d7fcf6ab0
claudioeberle <claudioeberle@gmail.com>
2018-03-26 12:01:48 -0300

    Add files via upload

---
+++ eberle/funciones/main.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+float sumar (int numero1, int numero2);
+void mostrar(void);
+void pedir(void);
+int leer (void);
+int pediryleer (int minimo, int maximo);
+char pediroperador (void);
+float calculo (int numero1, char operador, int numero2);
+
+int main()
+{
+    int num1;
+    char operador;
+    int num2;
+    float resultado;
+
+    mostrar();
+
+    num1=pediryleer(0, 100);
+    char pediroperador (void);
+    num2=pediryleer(0, 100);
+
+    printf("%d", num1);
+    printf("%c", operador);
+    printf("%d = ", num2);
+
+
+
+    resultado = calculo(num1, operador, num2);
+
+    printf("%f", resultado);
+
+    return 0;
+}
+
+float sumar (int numero1, int numero2)
+{
+
+    float resultado;
+
+    resultado = numero1 + numero2;
+
+    return resultado;
+
+}
+/** \brief
+ * realiza suma de dos numero
+ * \numero1 primer numero entero
+ * \numero2 segundo numero entero
+ * \return resultado de la suma
+ *
+ */
+
+
+void mostrar(void)
+{
+    printf("Hola mundo!\n\n");
+}
+
+void pedir (void)
+{
+    printf("Ingrese un número: ");
+}
+
+int leer(void)
+{
+    int numero;
+    fflush(stdin);
+    scanf("%d", &numero);
+    return numero;
+}
+
+int pediryleer (int minimo, int maximo)
+{
+    int retorno;
+
+    pedir();
+    retorno=leer();
+    while(retorno<minimo || retorno>maximo)
+    {
+        pedir();
+        retorno=leer();
+    }
+
+
+    return retorno;
+
+}
+
+char pediroperador (void) // pide operador valido entre (+ - *  /)
+{
+    char operador;
+    printf("\nIngrese operador: ");
+    operador=getche();
+
+    while(operador !='+' && operador !='-' && operador !='*' && operador !='/')
+    {
+            printf("\nIngrese operador: ");
+    }
+
+    return operador;
+
+}
+
+
+
+
+float calculo (int num1, char operador, int num2)
+{
+    float calculo;
+
+    switch(operador)
+    {
+        case '+': calculo = num1+num2;
+        break;
+
+        case '-': calculo = num1-num2;
+        break;
+
+        case '*': calculo = num1*num2;
+        break;
+
+        case '/': calculo = (float)num1/num2;
+        break;
+
+    }
+
+    return calculo;
+}
